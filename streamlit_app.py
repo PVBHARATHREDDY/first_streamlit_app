9b126f895360a97cfdc0c131d4983fc8e5067139
PVBHARATHREDDY <125216941+PVBHARATHREDDY@users.noreply.github.com>
2023-03-06 10:54:46 +0100

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title('I am the boss')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
